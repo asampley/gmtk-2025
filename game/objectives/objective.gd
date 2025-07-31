33bd36e9f03b8d6a0ffd25ed02a16ace280e31cb
dadames <dadames999@gmail.com>
2025-07-31 15:29:35 -0600

    Objectives can be completed

---
+++ game/objectives/objective.gd
@@ -0,0 +1,15 @@
+class_name Objective
+
+var template: ObjectiveTemplate
+var tasks_completed: int = 0
+var completed: bool
+var rewarded: bool
+
+
+func initialize(template_in: ObjectiveTemplate) -> void:
+	template = template_in
+
+func task_completed() -> void:
+	tasks_completed += 1
+	if tasks_completed >= template.number_of_tasks:
+		completed = true
--- game/objectives/objective_manager.gd
+++ game/objectives/objective_manager.gd
@@ -1,1 +1,20 @@
 extends Node
+
+
+var objectives: Dictionary[String, Objective] ={}
+
+
+func _ready() -> void:
+	load_objectives()
+
+func load_objectives() -> void:
+	for template: ObjectiveTemplate in DataHandler.objective_resources:
+		var objective := Objective.new()
+		objective.initialize(template)
+		objectives[template.title] = objective
+
+func on_objective_task_completed(objective_title: String) -> void:
+	if objectives.has(objective_title):
+		objectives[objective_title].task_completed()
+	else:
+		printerr("There is a mistitled objective task.")
