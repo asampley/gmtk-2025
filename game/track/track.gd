a12fed651750a812f88aa46609fc8ec77e87cf0b
Alex Sampley <alex.sampley@gmail.com>
2025-07-30 18:15:56 -0600

    Amend custom data to be more graph based allowing crossings

--- game/track/track.gd
+++ game/track/track.gd
@@ -10,14 +10,35 @@ func pixel_to_tile_coord(pixel: Vector2i) -> Vector2i:
 		pixel.y / tile_size.y - (0 if pixel.y >= 0 else 1),
 	)
 
-# Return the convergence *direction* for all branches.
+# Returns the possible output directions given an input direction.
 #
-# If moving onto the tile from this face, a branch can be selected.
-#
-# If moving onto the tile from another face, this face should be the output direction.
-func tile_branch_convergence(tile: Vector2i) -> Vector2i:
-	return self.get_cell_tile_data(tile).get_custom_data("start")
+# Direction is a unit Vector2i.
+func connections(tile: Vector2i, direction: Vector2i) -> Array[Vector2i]:
+	match direction:
+		Vector2i.UP:
+			return connections_up(tile)
+		Vector2i.DOWN:
+			return connections_down(tile)
+		Vector2i.LEFT:
+			return connections_left(tile)
+		Vector2i.RIGHT:
+			return connections_right(tile)
+		_:
+			# TODO should this error, or is an empty array catastrophic enough?
+			return []
 
-# Return a list of possible *directions*, not indexes.
-func tile_branches(tile: Vector2i) -> Array[Vector2i]:
-	return self.get_cell_tile_data(tile).get_custom_data("branches")
+# Returns the possible output directions entering from the top
+func connections_up(tile: Vector2i) -> Array[Vector2i]:
+	return self.get_cell_tile_data(tile).get_custom_data("conn_up")
+
+# Returns the possible output directions entering from the bottom
+func connections_down(tile: Vector2i) -> Array[Vector2i]:
+	return self.get_cell_tile_data(tile).get_custom_data("conn_down")
+
+# Returns the possible output directions entering from the bottom
+func connections_left(tile: Vector2i) -> Array[Vector2i]:
+	return self.get_cell_tile_data(tile).get_custom_data("conn_left")
+
+# Returns the possible output directions entering from the bottom
+func connections_right(tile: Vector2i) -> Array[Vector2i]:
+	return self.get_cell_tile_data(tile).get_custom_data("conn_right")
