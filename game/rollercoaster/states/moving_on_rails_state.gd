7af1fbb5c674e306ccabf2969c9e669b99544873
Alex Sampley <alex.sampley@gmail.com>
2025-07-31 12:42:55 -0600

    Refine physics for backwards going

--- game/rollercoaster/states/moving_on_rails_state.gd
+++ game/rollercoaster/states/moving_on_rails_state.gd
@@ -42,35 +42,55 @@ func process_frame(delta: float) -> State:
 func process_physics(delta: float) -> State:
 	if !base_node.is_on_floor():
 		return falling_state
-	if base_node.velocity.length() <= 10:
-		return stopped_state
-	#base_node.velocity.y += gravity * delta
-	#base_node.move_and_slide()
 
-	var new_progress := base_node.path_follow.progress + base_node.velocity.length() * delta
+	var curve := base_node.path.curve
+	var follow := base_node.path_follow
 
-	if new_progress > base_node.path.curve.get_baked_length():
-		new_progress -= base_node.path.curve.get_baked_length()
+	# if velocity is against the path, we should update it
+	var progress_direction := (curve.sample_baked(follow.progress_ratio + 0.05) - (curve.sample_baked(follow.progress_ratio - 0.05))).normalized()
+	if base_node.velocity.dot(progress_direction) < 0:
+		flip()
+
+	var new_progress := follow.progress + base_node.velocity.length() * delta
+
+	if new_progress > curve.get_baked_length():
+		new_progress -= curve.get_baked_length()
 
 		print("Leaving tile %s from %s" % [tile_pos, out_direction])
 		tile_pos += out_direction
 		print("Entering tile %s from %s" % [tile_pos, -out_direction])
 
 		in_direction = -out_direction
-		var state:=  update_path()
+		var state := update_path()
 		if state != null:
 			return state
 
-	var old_pos := self.global_position
+	var old_pos := base_node.global_position
 
 	base_node.path_follow.progress = new_progress
 	base_node.transform = base_node.path_follow.transform
 
-	var direction := (self.global_position - old_pos).normalized()
-	base_node.velocity = base_node.velocity.length() * direction \
-		+ (self.gravity * Vector2i.DOWN).project(direction) * delta
+	var direction := (base_node.global_position - old_pos).normalized()
+
+	var dv := (self.gravity * Vector2i.DOWN).project(direction) * delta
+	base_node.velocity = base_node.velocity.length() * direction + dv
+
+	# if velocity is against the path, we should update it
+	if base_node.velocity.dot(direction) < 0:
+		flip()
+
+	if base_node.velocity.length() <= 10 && dv.length_squared() <= 1 * delta:
+		return stopped_state
 
 	return null
+
+func flip() -> void:
+	var temp := in_direction
+	in_direction = out_direction
+	out_direction = temp
+	base_node.path_follow.progress_ratio = 1 - base_node.path_follow.progress_ratio
+	update_path()
+
 
 func get_direction(vector_in: Vector2) -> Vector2i:
 	return (Vector2i(-vector_in) / 32).clampi(-1,1)
