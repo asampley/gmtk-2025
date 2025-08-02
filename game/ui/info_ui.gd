777a658a5b2b954922605ca1a32d915746b9cca7
dadames <dadames999@gmail.com>
2025-08-02 01:06:08 -0600

    More scene stuff

--- game/ui/info_ui.gd
+++ game/ui/info_ui.gd
@@ -9,6 +9,8 @@ func _ready() -> void:
 	EventBus.money_amount_changed.connect(on_money_amount_changed)
 	EventBus.speed_update.connect(on_speed_update)
 	EventBus.airtime_changed.connect(on_airtime_changed)
+	EventBus.station_stop.connect(on_stopped_at_station)
+	EventBus.shop_menu_closed.connect(on_shop_menu_closed)
 
 func on_money_amount_changed(money: int) -> void:
 	money_text.text = "Money: %i" % money
@@ -18,3 +20,9 @@ func on_speed_update(speed: float) -> void:
 
 func on_airtime_changed(airtime: float) -> void:
 	airtime_text.text = "Airtime: %s s" % snappedf(airtime, 0.01)
+
+func on_stopped_at_station() -> void:
+	hide()
+
+func on_shop_menu_closed() -> void:
+	show()
