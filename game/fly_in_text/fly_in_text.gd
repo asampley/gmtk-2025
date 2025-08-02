316fb20b72de42f06e8a86a38ece3d15e2b51fde
dadames <dadames999@gmail.com>
2025-08-02 02:24:24 -0600

    Fixed fly in text

--- game/fly_in_text/fly_in_text.gd
+++ game/fly_in_text/fly_in_text.gd
@@ -1,20 +1,30 @@
 extends Label
 
-
+var new_material: Material = preload("res://game/fly_in_text/fly_in_text_shader_material_prefab.tres")
+var new_shader: Shader = preload("res://game/shaders/directional_move.gdshader")
 @export var fade_time: float = 0.5
 
+var speed: float
+var direction: Vector2
 var duration: float = 2
 var fading := false
 
 
-func initialize(text_in: String) -> void:
+func initialize(text_in: String, speed_in: float, direction_in: Vector2) -> void:
+	EventBus.station_stop.connect(on_stop_entered)
 	text = text_in
+	speed = speed_in
+	direction = direction_in
 	await get_tree().create_timer(duration).timeout
 	fading = true
 
 func _process(delta: float) -> void:
+	position += direction * speed * delta
 	if !fading:
 		return
 	modulate.a -= 1 / fade_time * delta
 	if modulate.a <= 0:
 		queue_free()
+
+func on_stop_entered() -> void:
+	queue_free()
--- game/fly_in_text/fly_in_text_manager.gd
+++ game/fly_in_text/fly_in_text_manager.gd
@@ -13,9 +13,4 @@ func on_generated_fly_in_text(text: String, position: Vector2, direction: Vector
 	var fly_in_text: Label = fly_in_text_prefab.instantiate()
 	fly_in_text_parent.add_child(fly_in_text)
 	fly_in_text.position = position
-	fly_in_text.initialize(text)
-	print(fly_in_text.material.get_rid())
-	fly_in_text.material = fly_in_text.material.duplicate()
-	print(fly_in_text.material.get_rid())
-	fly_in_text.material.set("shader_parameter/speed", text_speed)
-	fly_in_text.material.set("shader_parameter/direction", direction)
+	fly_in_text.initialize(text, text_speed, direction)
