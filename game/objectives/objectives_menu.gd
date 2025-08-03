86b20284c7aac6843b6d958f179e32a5b5fef47a
dadames <dadames999@gmail.com>
2025-08-03 00:59:25 -0600

    Sort completed objectives to top

--- game/objectives/objectives_menu.gd
+++ game/objectives/objectives_menu.gd
@@ -11,8 +11,13 @@ func _ready() -> void:
 func on_objective_menu_requested() -> void:
 	for child: Node in objective_item_parent.get_children():
 		child.queue_free()
+	var completed_nodes: Array[Node]
 	for objective: Objective in Globals.objectives.values():
 		if !objective.claimed:
 			var objective_item := objective_item_prefab.instantiate()
 			objective_item_parent.add_child(objective_item)
+			if objective.completed:
+				completed_nodes.append(objective_item)
 			objective_item.initialize(objective)
+	for node: Node in completed_nodes:
+		objective_item_parent.move_child(node, 0)
