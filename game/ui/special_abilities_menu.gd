b3d9bc244e78595735b06c7a712cff7391ed1cdd
dadames <dadames999@gmail.com>
2025-08-02 21:31:43 -0600

    Nitro stuff

--- game/ui/special_abilities_menu.gd
+++ game/ui/special_abilities_menu.gd
@@ -7,6 +7,8 @@ extends Control
 func _ready() -> void:
 	EventBus.glide_cooldown_changed.connect(on_glide_cooldown_changed)
 	EventBus.nitro_cooldown_changed.connect(on_nitro_cooldown_changed)
+	EventBus.station_stop.connect(on_need_hide)
+	EventBus.shop_menu_closed.connect(on_need_show)
 
 func on_glide_cooldown_changed(amount: float) -> void:
 	glide_cooldown.value = amount
@@ -14,4 +16,9 @@ func on_glide_cooldown_changed(amount: float) -> void:
 func on_nitro_cooldown_changed(amount: float) -> void:
 	print(amount)
 	nitro_cooldown.value = amount
-	
+
+func on_need_hide() -> void:
+	hide()
+
+func on_need_show() -> void:
+	show()
