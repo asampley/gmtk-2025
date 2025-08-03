95628567b0c12a8ae6930002501bd92caa15c960
dadames <dadames999@gmail.com>
2025-08-03 09:13:58 -0600

    Fixed Combo Objectives

--- game/objective_listener.gd
+++ game/objective_listener.gd
@@ -5,8 +5,17 @@ func _ready() -> void:
 
 func on_combo_completed(combo_name: String, _current_score: float, _current_mult: float) -> void:
 	match combo_name:
-		"Front Flip":
-			EventBus.objective_task_completed.emit("Do a Front Flip")
+		"Back Flip":
+			EventBus.objective_task_completed.emit("Do a Back Flip")
 		"Barrel Roll":
 			EventBus.objective_task_completed.emit("Do a Barrel Roll")
+		"Front Flip":
+			EventBus.objective_task_completed.emit("Do a Front Flip")
+		"Kick Flip":
+			EventBus.objective_task_completed.emit("Do a Kick Flip")
+		"Heel Flip":
+			EventBus.objective_task_completed.emit("Do a Heel Flip")
+		"Flat Spin":
+			EventBus.objective_task_completed.emit("Do a Flat Spin")
+		
 		
