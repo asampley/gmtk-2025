9153c44dd2571f8df2cf7eebaecc151805fa3c5b
dadames <dadames999@gmail.com>
2025-08-07 22:22:13 -0600

    Improved screen shake and made it scale with zoom

--- game/screen_shake/screen_shake.gd
+++ game/screen_shake/screen_shake.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
 
-@export var shake_multiplier: float = 1.5
+@export var shake_divisor: float = 1.5
 @export var max_shake: float = 1000
+@export var base_shake_fade: float = 1.0
 var shake_strength: float = 0
-var base_shake_fade: float = 1
+
 
 var camera: Camera2D
 
@@ -21,9 +22,11 @@ func _process(delta: float) -> void:
 		camera.offset = Vector2(0, 0)
 
 func on_screen_shake_increased(intensity: float) -> void:
-	shake_strength += intensity * shake_multiplier
+	shake_strength += intensity / shake_divisor
 	shake_strength = clampf(shake_strength, 0, max_shake)
 
 func random_offset() -> Vector2:
 	var rng := RandomNumberGenerator.new()
-	return Vector2(rng.randf_range(-shake_strength, shake_strength),rng.randf_range(-shake_strength, shake_strength))
+	var x_offset := rng.randf_range(-shake_strength / camera.zoom.x, shake_strength / camera.zoom.x)
+	var y_offset := rng.randf_range(-shake_strength / camera.zoom.y, shake_strength / camera.zoom.y)
+	return Vector2(x_offset, y_offset)
