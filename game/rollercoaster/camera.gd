e3a9743c1d44f525c73c7519921d04abc7dcb18c
dadames <dadames999@gmail.com>
2025-08-07 22:10:18 -0600

    Camera zoom can be logarithmic

--- game/rollercoaster/camera.gd
+++ game/rollercoaster/camera.gd
@@ -1,6 +1,6 @@
 extends Camera2D
 
-
+@export var logarithmic_zoom_scaling: bool
 @export var min_zoom: float
 @export var max_zoom: float
 @export var max_speed: float
@@ -10,8 +10,21 @@ var base_node: CharacterBody2D
 func initialize(base_node_in: CharacterBody2D) -> void:
 	base_node = base_node_in
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
+	if logarithmic_zoom_scaling:
+		logarithmic_scaling()
+	else:
+		linear_scaling()
+
+func linear_scaling() -> void:
 	var current_speed := base_node.velocity.length()
 	var zoom_multiplier := current_speed / max_speed
 	var zoom_amount: float = clamp(min_zoom / zoom_multiplier, min_zoom, max_zoom)
+	zoom = Vector2(zoom_amount, zoom_amount)
+
+func logarithmic_scaling() -> void:
+	var current_speed := base_node.velocity.length()
+	var zoom_multiplier := current_speed / max_speed
+	var logarithmic_zoom_multiplier := log(1.0 + (zoom_multiplier * 9.0)) / log(10.0)
+	var zoom_amount: float = clamp(min_zoom / logarithmic_zoom_multiplier, min_zoom, max_zoom)
 	zoom = Vector2(zoom_amount, zoom_amount)
